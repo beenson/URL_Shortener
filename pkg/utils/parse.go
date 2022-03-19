3dd10c0546f83c1be33c68e7aa3f2145dbfb89e3
beenson <benson070707@yahoo.com.tw>
2022-03-19 13:16:02 +0800

    Fix validation error messages are more readable

--- pkg/utils/parse.go
+++ pkg/utils/parse.go
@@ -2,6 +2,8 @@ package util
 
 import (
 	"errors"
+	"reflect"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
@@ -13,10 +15,28 @@ func ParseAndValidate(c *fiber.Ctx, out interface{}) error {
 		return err
 	}
 
+	// Create validator
+	validate := validator.New()
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+
 	// Validate
-	if err := validator.New().Struct(out); err != nil {
+	if err := validate.Struct(out); err != nil {
 		validationError := err.(validator.ValidationErrors)[0]
-		return errors.New(validationError.Field() + ": " + validationError.Tag())
+		conj := ""
+		if validationError.Tag() == "required" {
+			conj = " is "
+		} else {
+			conj = " should be "
+		}
+		return errors.New(validationError.Field() + conj + validationError.Tag())
 	}
 
 	return nil
